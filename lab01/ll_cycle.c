a7141c3b795bc52afddf86b5ed0a65bc682012a2
laity <3216100019@qq.com>
2024-02-01 20:14:31 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,16 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+    node *tortoise = head, *hare = head;
+    while (hare->next != NULL && hare->next->next != NULL) {
+	hare = hare->next->next;
+	tortoise = tortoise->next;
+	if (tortoise == hare) {
+	    return 1;
+	}
+    }
     return 0;
 }
